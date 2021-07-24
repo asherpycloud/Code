e2fd75b2f9539d2c889fd7ce5fd8fcb25a2d53c2
WhatheHelen <67233497+WhatheHelen@users.noreply.github.com>
2021-07-24 11:19:52 -0400

    hallo

---
+++ src/InClassExercise1.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class InClassExercise1 {
+    public static void main(String[] arg){
+        String[] in = new String[14];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i<=13; i++){
+            in[i] = sc.nextLine().toLowerCase();
+        }
+
+        for(int i = 0; i<=13; i++){
+            System.out.println(in[i]);
+        }
+    }
+}
